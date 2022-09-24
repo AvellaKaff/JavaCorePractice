c03fe001a14f5bba1f02f2014e788e75928e2fa5
AvellaKaff <91131025+AvellaKaff@users.noreply.github.com>
2022-09-24 13:55:08 +0400

    Урок 5. Средства ввода-вывода

---
+++ Main.java
@@ -0,0 +1,14 @@
+package main.java.lesson5;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        AppData data = new AppData();
+        data.readFromFile("src/file.csv");
+        System.out.println(Arrays.toString(data.getHeader()));
+        System.out.println(Arrays.deepToString(data.getData()));
+
+        data.writeToCSV("new_example.csv");
+    }
+}
